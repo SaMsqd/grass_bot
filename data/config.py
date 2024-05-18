40ec3591d3864bff0216038cad81ed7d6f88aa13
SaM-laptop <arh1pov.sam@yandex.ru>
2024-05-18 13:09:33 +0300

    proxy and accounts

--- data/config.py
+++ data/config.py
@@ -2,7 +2,7 @@ MIN_PROXY_SCORE = 50
 
 CLAIM_REWARDS_ONLY = False  # claim tiers rewards only (https://app.getgrass.io/dashboard/referral-program)
 
-THREADS = 2  # for register account / claim rewards mode only
+THREADS = 6  # for register account / claim rewards mode only
 
 CHECK_POINTS = True  # show point for each account every nearly 10 minutes
 
@@ -13,7 +13,7 @@ SHOW_LOGS_RARELY = False
 
 # REGISTER PARAMETRS ONLY
 REGISTER_ACCOUNT_ONLY = False
-REGISTER_DELAY = (3, 7)
+REGISTER_DELAY = (1, 3)
 
 TWO_CAPTCHA_API_KEY = ""
 ANTICAPTCHA_API_KEY = ""
